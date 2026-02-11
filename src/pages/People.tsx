4c791b1fa194145421df0fa741df349a54d88f76
Seobin-Biomech <awdrg77@gmail.com>
2026-02-11 19:31:07 +0900

    Update People.tsx

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -9,7 +9,6 @@ import imgSia from "../assets/siamoon.jpg";
 import imgSeunghwan from "../assets/SeunghwanKim.jpg";
 import imgJonggyu from "../assets/JonggyuYun.jpg";
 import scholarIcon from "../assets/googlescholar.logo.png";
-import linkedinIcon from "../assets/linkedin.logo.png";
 
 /* ------- StudentCard (사이트 CSS 클래스 사용) ------- */
 type StudentCardProps = {
@@ -96,7 +95,7 @@ function StudentCard({
             className="icon-link"
             title="LinkedIn"
           >
-            <img src={linkedinIcon} alt="LinkedIn" className="icon-20" />
+          <svg className="icon-20" width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
           </a>
         )}
       </div>
@@ -163,13 +162,13 @@ export default function People() {
               </a>
 
               <a
-                href="https://www.linkedin.com/in/seobinchoi/?locale=ko_KR"
+href="https://www.linkedin.com/in/seobinchoi/?locale=ko_KR"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="LinkedIn"
                 title="LinkedIn"
               >
-                <img src={linkedinIcon} className="icon-28" alt="LinkedIn" />
+                <svg className="icon-28" width="28" height="28" viewBox="0 0 24 24" fill="currentColor"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
               </a>
             </div>
           </div>
