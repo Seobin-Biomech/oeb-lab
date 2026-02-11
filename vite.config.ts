1acc908f5b9609751e26f15e8853eefcc9a2f362
Seobin-Biomech <awdrg77@gmail.com>
2026-02-11 19:37:53 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/oeblab/",   // ✅ 레포 이름
+  base: "/oeb-lab/",   // ✅ 레포 이름
 });
